d63abd372af4409ab56ecf896bf4c3b8025decde
Kenzi Mohamed <kenzimohamed476@gmail.com>
2026-05-08 05:17:38 +0300

    fix vercel deployment

--- server.js
+++ server.js
@@ -37,7 +37,4 @@ mongoose.connection.once("open", () => {
 });
 
 // server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
