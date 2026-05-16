bbc3db7c6c83a1c45ab1e7515e4cd758f5f4b5f7
Kenzi Mohamed <kenzimohamed476@gmail.com>
2026-05-16 23:27:45 +0300

    update project similarity flow

--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -2,11 +2,10 @@ const express = require("express");
 const router = express.Router();
 const teamController = require("../controllers/teamController");
 const verifyToken = require("../middleware/auth");
-
+router.put("/leave", verifyToken, teamController.leaveTeam);
 router.post("/create", verifyToken, teamController.createTeam);
 router.post("/add-member", verifyToken, teamController.addMember);
 router.get("/no-project", verifyToken, teamController.getTeamsWithoutProject);
-router.put("/leave",auth,teamController.leaveTeam);
 router.post("/join-request", verifyToken, teamController.sendJoinRequest);
 router.get("/requests", verifyToken, teamController.getTeamRequests);
 router.post("/handle-request", verifyToken, teamController.handleRequest);
