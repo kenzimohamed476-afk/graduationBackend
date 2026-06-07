e036fd8549b74a3b6c7c07b4a5a49196acd7474e
Kenzi Mohamed <kenzimohamed476@gmail.com>
2026-06-07 23:58:58 +0300

    add fcm token endpoint

--- utils/sendNotification.js
+++ utils/sendNotification.js
@@ -1,4 +1,6 @@
 const Notification = require("../models/notification");
+const User = require("../models/user");
+const sendPushNotification = require("./sendPushNotification");
 
 const sendNotification = async (
   user_id,
@@ -6,11 +8,24 @@ const sendNotification = async (
   message
 ) => {
   try {
+    // Save notification in database
     await Notification.create({
       user_id,
       title,
       message,
     });
+
+    // Get user
+    const user = await User.findById(user_id);
+
+    // Send push notification if token exists
+    if (user?.fcm_token) {
+      await sendPushNotification(
+        user.fcm_token,
+        title,
+        message
+      );
+    }
   } catch (err) {
     console.log(err);
   }
