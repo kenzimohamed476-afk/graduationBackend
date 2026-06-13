12de6f90155e8e8c4d92d12a54378385f6f4ab0c
Kenzi Mohamed <kenzimohamed476@gmail.com>
2026-06-13 18:35:12 +0300

    add library dashboard

--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -3,20 +3,10 @@ const router = express.Router();
 const teamController = require("../controllers/teamController");
 const verifyToken = require("../middleware/auth");
 router.put("/leave", verifyToken, teamController.leaveTeam);
-
 router.post("/add-member", verifyToken, teamController.addMember);
 router.get("/no-project", verifyToken, teamController.getTeamsWithoutProject);
 router.post("/join-request", verifyToken, teamController.sendJoinRequest);
 router.get("/requests", verifyToken, teamController.getTeamRequests);
 router.post("/handle-request", verifyToken, teamController.handleRequest);
-router.get(
-  "/find-teams",
-  verifyToken,
-  teamController.findTeams
-);
-router.get(
-  "/:id",
-  verifyToken,
-  teamController.getTeamDetails
-);
+
 module.exports = router;
