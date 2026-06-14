4cf9031f02851f8789708018fdfc5d473147e7f4
Kenzi Mohamed <kenzimohamed476@gmail.com>
2026-06-14 04:41:26 +0300

    add library dashboard

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -16,18 +16,6 @@ router.get(
   getNotifications
 );
 
-// Get unread notifications count
-router.get(
-  "/unread-count",
-  auth,
-  getUnreadCount
-);
 
-// Mark notification as read
-router.patch(
-  "/:id/read",
-  auth,
-  markAsRead
-);
 
 module.exports = router;
